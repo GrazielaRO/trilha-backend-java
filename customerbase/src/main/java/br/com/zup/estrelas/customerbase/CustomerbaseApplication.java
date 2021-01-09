5e7873dcffd015cb5ab02b14a50966dc58bfa1df
GrazielaRO <grazi375@hotmail.com>
2021-01-09 10:41:14 -0300

    Project created

---
+++ customerbase/src/main/java/br/com/zup/estrelas/customerbase/CustomerbaseApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelas.customerbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerbaseApplication.class, args);
+	}
+
+}
