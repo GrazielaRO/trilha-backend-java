074fdc5dc06221cd74907ca954a19a024a85093c
GrazielaRO <grazi375@hotmail.com>
2021-01-19 17:22:16 -0300

    log implementation

--- customerbase/src/main/java/br/com/zup/estrelas/customerbase/exceptions/GlobalExceptionHandler.java
+++ customerbase/src/main/java/br/com/zup/estrelas/customerbase/exceptions/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import static java.util.Objects.nonNull;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -17,6 +19,8 @@ import br.com.zup.estrelas.customerbase.dto.ErrorDTO;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+	
+	private static final Logger logger = LogManager.getLogger(GlobalExceptionHandler.class);
 	
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -38,19 +42,21 @@ public class GlobalExceptionHandler {
                 validationErrors.add(new ErrorDTO(messageDisplayed.toString()));
             }
         }
-
+        logger.error("VALIDATION ERROR.");
         return validationErrors;
     }
 	
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ExceptionHandler({ NotFoundException.class })
 	public @ResponseBody ErrorDTO handlerBusinessRules(NotFoundException exception) {
+		logger.error("CUSTOMER NOT FOUND.");
 		return new ErrorDTO(exception.getMessage());
 	}
 	
 	@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
 	@ExceptionHandler({ CustomerAlreadyExistsException.class })
 	public @ResponseBody ErrorDTO handlerBusinessRules(CustomerAlreadyExistsException exception) {
+		logger.error("CUSTUMER ALREADY EXISTS IN THE DATABASE.");
 		return new ErrorDTO(exception.getMessage());
 	}
 
