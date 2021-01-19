30f17e1100c97dcdd5b2006a6686f58c0a1560f0
GrazielaRO <grazi375@hotmail.com>
2021-01-19 10:52:08 -0300

    Swagger implementation

--- customerbase/src/main/java/br/com/zup/estrelas/customerbase/controllers/CustomerController.java
+++ customerbase/src/main/java/br/com/zup/estrelas/customerbase/controllers/CustomerController.java
@@ -20,7 +20,10 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.zup.estrelas.customerbase.dto.CustomerDTO;
 import br.com.zup.estrelas.customerbase.entities.Customer;
 import br.com.zup.estrelas.customerbase.services.CustomerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
+@Api
 @RestController
 @RequestMapping ("/customers")
 public class CustomerController {
@@ -28,30 +31,35 @@ public class CustomerController {
 	@Autowired
 	CustomerService service;
 	
+	@ApiOperation(value="Create a new customer")
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping
 	public String isert(@Valid @RequestBody CustomerDTO insertCustomerDTO){
 		return service.insert(insertCustomerDTO);
 	}
 	
+	@ApiOperation(value="List all customers")
 	@ResponseStatus(HttpStatus.OK)
 	@GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
 	public List<Customer> findAll(){
 		return service.findAll();
 	}
 	
+	@ApiOperation(value="Find a customer")
 	@ResponseStatus(HttpStatus.OK)
 	@GetMapping(path = "/{cpf}", produces = {MediaType.APPLICATION_JSON_VALUE})
 	public Customer find (@PathVariable String cpf){
 		return service.find(cpf);
 	}
 	
+	@ApiOperation(value="Update a customer data")
 	@ResponseStatus(HttpStatus.OK)
 	@PutMapping(path = "/{cpf}")
 	public String updateData (@PathVariable String cpf, @Valid @RequestBody CustomerDTO customerDTO){
 		return service.updateData(cpf, customerDTO);
 	}
 	
+	@ApiOperation(value="Delete a customer")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@DeleteMapping(path = "/{cpf}")
 	public void delete (@PathVariable String cpf){
